b12e6b284ee77f8dc52b89523a904473f97e0b6e
Andreas Schneider <asn@cryptomilk.org>
2012-02-02 19:07:23 +0100

    Update product defines.

--- cm.mk
+++ cm.mk
@@ -8,10 +8,10 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := marvel
-PRODUCT_NAME := cm_marvel
-PRODUCT_BRAND := HTC
-PRODUCT_MODEL := Wildfire S
+PRODUCT_NAME := htc_marvel
+PRODUCT_BRAND := htc_europe
+PRODUCT_MODEL := HTC Wildfire S A510e
 PRODUCT_MANUFACTURER := HTC
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=marvel BUILD_ID=ASN01 BUILD_DISPLAY_ID=ASN01 BUILD_FINGERPRINT=google/htc/marvel:2.3.7/ASN01/0.1:user/release-keys PRIVATE_BUILD_DESC="4.08.605.3 CL120826 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=marvel BUILD_ID=ASN01 BUILD_DISPLAY_ID=ASN01 BUILD_FINGERPRINT=cyanogenmod/htc_marvel/marvel:4.0.3/ASN01/0.1:user/release-keys PRIVATE_BUILD_DESC="2.13.401.2 CL197017 release-keys"
