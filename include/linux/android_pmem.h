b94d78e9e593a180d4067d6f38a66c9d0157d5f5
Andreas Schneider <asn@cryptomilk.org>
2012-04-02 17:07:40 +0200

    include: Fix latest gralloc build.

--- include/linux/android_pmem.h
+++ include/linux/android_pmem.h
@@ -12,6 +12,8 @@
 #ifndef _ANDROID_PMEM_H_
 #define _ANDROID_PMEM_H_
 
+#include <linux/fs.h>
+
 #define PMEM_IOCTL_MAGIC 'p'
 #define PMEM_GET_PHYS _IOW(PMEM_IOCTL_MAGIC, 1, unsigned int)
 #define PMEM_MAP _IOW(PMEM_IOCTL_MAGIC, 2, unsigned int)
